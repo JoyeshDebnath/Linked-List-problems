7818a3a9d56726086010cc2f626541a8eea9472e
saikatXshrey <76695320+saikatXshrey@users.noreply.github.com>
2021-01-11 11:38:08 +0530

    Add files via upload

---
+++ Linked List/Single Linked List/swap/swap in pairs/changing links/swap_efficient.java
@@ -0,0 +1,67 @@
+//Pair wise swapping of linked list by changing the links
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        next = null;
+    }
+}
+
+class Link {
+    Node head;
+
+    void push(Node new_node) {
+        if (head == null) {
+            head = new_node;
+            return;
+        }
+        Node temp = head;
+        while (temp.next != null)
+            temp = temp.next;
+
+        temp.next = new_node;
+    }
+
+    void display(Node ref) {
+        while (ref != null) {
+            System.out.print(ref.data + "-->");
+            ref = ref.next;
+        }
+        System.out.println("null");
+    }
+
+    Node swapNode(Node listhead) {
+        if (listhead == null || listhead.next == null)
+            return listhead;
+        Node curr = listhead.next.next;
+        Node prev = listhead;
+        listhead = listhead.next;
+        listhead.next = prev;
+        while (curr != null && curr.next != null) {
+            prev.next = curr.next;
+            prev = curr;
+            Node next = curr.next.next;
+            curr.next.next = curr;
+            curr = next;
+        }
+        prev.next = curr;
+        return listhead;
+    }
+
+    public static void main(String[] args) {
+        Link list = new Link();
+
+        list.push(new Node(1));
+        list.push(new Node(2));
+        list.push(new Node(3));
+        list.push(new Node(4));
+        list.push(new Node(5));
+
+        list.display(list.head);
+
+        list.display(list.swapNode(list.head));
+    }
+}
---
+++ Linked List/Single Linked List/swap/swap in pairs/swapping data only/brute.java
@@ -0,0 +1,62 @@
+/*Pairwise swap nodes of linked list only by swapping data*/ */
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        next = null;
+    }
+}
+
+class Link {
+    Node head;
+
+    void push(Node new_node) {
+        if (head == null) {
+            head = new_node;
+            return;
+        }
+        Node temp = head;
+        while (temp.next != null)
+            temp = temp.next;
+
+        temp.next = new_node;
+    }
+
+    void display(Node ref) {
+        while (ref != null) {
+            System.out.print(ref.data + "-->");
+            ref = ref.next;
+        }
+        System.out.println("null");
+    }
+
+    Node swap(Node l) {
+        int temp = 0;
+        Node res = l;
+
+        while (l != null && l.next != null) {
+            temp = l.data;
+            l.data = l.next.data;
+            l.next.data = temp;
+            l = l.next.next;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Link list = new Link();
+
+        list.push(new Node(1));
+        list.push(new Node(2));
+        list.push(new Node(3));
+        list.push(new Node(4));
+        list.push(new Node(5));
+
+        list.display(list.head);
+
+        list.display(list.swap(list.head));
+    }
+}
